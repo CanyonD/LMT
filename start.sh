7fd9fea9a55a26884df8dd98e1f1ef4cbec186ee
CanyonD8 <canyond8@gmail.com>
2017-09-24 23:02:30 +0300

    Added start script for reload git and

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /opt/LMT
+git pull >> /root/gitlog/update.log 2>&1
+git push >> /root/gitlog/update.log 2>&1
+service mongod restart
+
+int=$(ps aux | grep 'node server/server.js' | wc -l)
+if [ $int -ne 1 ];
+then
+    pkill node
+fi
+npm start >> system.log 2>&1 &
+
