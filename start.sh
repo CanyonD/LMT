29fe9dda9e2b935b40a99dc9e9f3ff0c76c1790d
Canyon Di <CanyonD8@gmail.com>
2017-09-25 22:22:11 +0300

    Changed path to npm and added refresh node modules

--- start.sh
+++ start.sh
@@ -7,16 +7,8 @@ service mongod restart
 int=$(ps aux | grep 'node server/server.js' | wc -l)
 if [ $int -ne 2 ];
 then
-    echo $int >> system.log 2>&1
     /usr/bin/pkill node
     /usr/bin/pkill npm
 fi
-<<<<<<< HEAD
- echo 'npm starting...' >> /opt/LMT/system.log 2>&1
+ sudo /usr/local/bin/npm install > /dev/null 2>&1
  sudo /usr/local/bin/npm start >> /opt/LMT/system.log 2>&1 &
- 
-=======
-/usr/local/bin/npm install > /dev/null 2>&1
-/usr/local/bin/npm start >> system.log 2>&1 &
-
->>>>>>> c2ccd7f10e9059829e6226bc6e3cadfe208e1938
