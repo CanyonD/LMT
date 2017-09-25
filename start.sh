5a07fcd86b5c2c7de2703db5648e9c069a741aaa
Canyon Di <CanyonD8@gmail.com>
2017-09-25 22:24:56 +0300

    Fixed killing process

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ git push >> /root/gitlog/update.log 2>&1
 service mongod restart
 
 int=$(ps aux | grep 'node server/server.js' | wc -l)
-if [ $int -ne 2 ];
+if [ $int -ne 1 ];
 then
     /usr/bin/pkill node
     /usr/bin/pkill npm
